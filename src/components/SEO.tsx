67e98ebe8ee83982040158b3fe1a16972416d43e
Ravi S. Rāmphal <rramphal@gmail.com>
2022-04-13 07:01:52 -0500

    updated SEO format

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -1,16 +1,65 @@
 import Head from 'next/head';
 
 type SEOProps = {
-  title: string;
+  cover?: string;
+  coverAltText?: string;
+  description?: string;
+  title?: string;
+  url?: string;
 }
 
 const SEO = (props: SEOProps) => {
-  const { title } = props;
+  const {
+    cover,
+    coverAltText,
+    description,
+    title,
+    url,
+  } = props;
+
+  const siteName            = 'NESAR';
+  const siteUrl             = 'https://nesar.org';
+  const siteDescription     = 'UPDATE_THIS_DESCRIPTION';
+  const siteLogo            = `${siteUrl}/assets/images/logo.png`;
+  const siteLogoDescription = 'The logo for the NESAR journal.'
+
+  // set up defaults
+  const tagDescription  = description || siteDescription;
+  const tagImageAltText = coverAltText || siteLogoDescription;
+  const tagImageUrl     = cover || siteLogo;
+  const tagTitle        = title ? `${title} | ${siteName}` : siteName;
+  const tagUrl          = url || siteUrl;
+
   return (
     <>
       <Head>
-        <title>{title} | NESAR</title>
-        <meta name="description" content="UPDATE_THIS_DESCRIPTION" />
+        {/* Standard */}
+        <title>{tagTitle}</title>
+        <meta name="title" content={tagTitle} />
+        <meta name="description" content={tagDescription} />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={tagUrl} />
+        <meta property="og:title" content={tagTitle} />
+        <meta property="og:description" content={tagDescription} />
+        <meta property="og:image" content={tagImageUrl} />
+        <meta property="og:image:alt" content={tagImageAltText} />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={tagUrl} />
+        <meta property="twitter:title" content={tagTitle} />
+        <meta property="twitter:description" content={tagDescription} />
+        <meta property="twitter:image" content={tagImageUrl} />
+        <meta property="twitter:image:alt" content={tagImageAltText} />
+
+        {/* Other Meta Tags */}
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#19191C" />
+        <link rel="canonical" href={tagUrl} />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
     </>
