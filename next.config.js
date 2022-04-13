0c33ee8eff2ce048dc64311734a97f051fb6498d
Ravi S. Rāmphal <rramphal@gmail.com>
2022-04-13 17:36:24 -0500

    added security headers

--- next.config.js
+++ next.config.js
@@ -1,9 +1,61 @@
-/** @type {import('next').NextConfig} */
+const ContentSecurityPolicy = [
+  `default-src 'self';`,
+  `script-src 'self';`,
+  // `child-src example.com;`,
+  // `style-src 'self' example.com;`,
+  `font-src 'self';`,
+].join('\n').replace(/\s{2,}/g, ' ').trim();
 
+// https://nextjs.org/docs/advanced-features/security-headers
+const globalHeaders = [
+  { // disable Google FLoC
+    'key': 'Permissions-Policy',
+    'value': 'interest-cohort=()',
+  },
+  { // enable DNS prefetching
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  { // only use HTTPS
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'X-XSS-Protection',
+    value: '1; mode=block',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'SAMEORIGIN',
+  },
+  { // browser permissions
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy,
+  },
+];
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
 
   reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        'source': '/(.*)',
+        'headers': globalHeaders
+      },
+    ];
+  },
 
   images: {
     formats: ['image/avif', 'image/webp'],
