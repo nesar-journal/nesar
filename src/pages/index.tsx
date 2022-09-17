2ccf480a870e9e4bddcfb3d4010523981027bbde
Andrew Ollett <andrew.ollett@gmail.com>
2022-09-17 08:52:24 -0500

    added hin (Hindi)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,45 +5,45 @@ import Layout from '../components/Layout';
 import SEO from '../components/SEO';
 
 const HomePage: NextPage = () => {
-    return (
-        <>
-            <Layout largeCover>
-                <SEO
-                    title="Home"
-                />
+  return (
+    <>
+      <Layout largeCover>
+        <SEO
+          title="Home"
+        />
 
-                <p><i>New Explorations in South Asia Research</i>, or <i>NESAR</i> (<span class="scriptWrapper" data-lang="kan" data-script="Latn" data-original="nesaṟ">nesaṟ</span> being a Kannada word for “sun”) is a totally <b>free</b> and <b>open-access</b> journal publishing original research of the highest quality in South Asian Studies, with a focus on the intellectual and expressive traditions of South India.</p>
+        <p><i>New Explorations in South Asia Research</i>, or <i>NESAR</i> (<span className="scriptWrapper" data-lang="kan" data-script="Latn" data-original="nesaṟ">nesaṟ</span> being a Kannada word for “sun”) is a totally <b>free</b> and <b>open-access</b> journal publishing original research of the highest quality in South Asian Studies, with a focus on the intellectual and expressive traditions of South India.</p>
 
-                <Heading
-                    level={2}
-                    titleEnd={1}
-                >
+        <Heading
+          level={2}
+          titleEnd={1}
+        >
                     Hero Article
-                </Heading>
+        </Heading>
 
-                {/* hero article */}
+        {/* hero article */}
 
-                <Heading
-                    level={2}
-                    titleEnd={1}
-                >
+        <Heading
+          level={2}
+          titleEnd={1}
+        >
                     Featured Article
-                </Heading>
+        </Heading>
 
-                {/*   */}
+        {/*   */}
 
-                <Heading
-                    level={2}
-                    titleEnd={1}
-                >
+        <Heading
+          level={2}
+          titleEnd={1}
+        >
                     Latest Articles
-                </Heading>
+        </Heading>
 
-                {/* dynamically generate most recent  */}
-                {/* exclude featured article  */}
-            </Layout>
-        </>
-    );
+        {/* dynamically generate most recent  */}
+        {/* exclude featured article  */}
+      </Layout>
+    </>
+  );
 };
 
 export default HomePage;
