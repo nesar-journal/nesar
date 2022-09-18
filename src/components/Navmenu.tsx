b75317d2fc8008fa5e23cb93f0ca4f6b711bec30
Ravi S. Rāmphal <rramphal@gmail.com>
2022-09-18 13:36:41 -0500

    wire up toggle for mobile navmenu and adjust background styling

--- src/components/Navmenu.tsx
+++ src/components/Navmenu.tsx
@@ -1,13 +1,20 @@
+import { useState } from 'react';
+
 import Link from 'next/link';
 
-import { menuItems } from './Navbar';
+import classnames from 'classnames';
+
 import TransliterationToggle from './TransliterationToggle';
+
+import { menuItems } from './Navbar';
 
 import styles from './Navmenu.module.scss';
 
 type NavmenuProps = {}
 
 const Navmenu = (props: NavmenuProps) => {
+  const [ isMenuOpen, setIsMenuOpen ] = useState(false);
+
   function renderLinks () {
     return menuItems.map((item) => {
       const { link, text } = item;
@@ -22,19 +29,45 @@ const Navmenu = (props: NavmenuProps) => {
     });
   }
 
+  function renderMenuToggle () {
+    return (
+      <div
+        className={styles.navmenuToggle}
+        role="button"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+      >
+        <div className={classnames(styles.navmenuToggleIcon, {
+          [styles.navmenuOpen]   : isMenuOpen,
+          [styles.navmenuClosed] : !isMenuOpen,
+        })}/>
+      </div>
+    );
+  }
+
+  function renderNavmenu () {
+    if (isMenuOpen) {
+      return (
+        <nav className={styles.navmenu}>
+          <div className={styles.menu}>
+            <ul className={styles.menuList}>
+              {renderLinks()}
+            </ul>
+          </div>
+
+          <div className={styles.transliterationToggle}>
+            <TransliterationToggle />
+          </div>
+        </nav>
+      );
+    }
+
+    return null;
+  }
+
   return (
     <>
-      <nav className={styles.navmenu}>
-        <div className={styles.menu}>
-          <ul className={styles.menuList}>
-            {renderLinks()}
-          </ul>
-        </div>
-
-        <div className={styles.transliterationToggle}>
-          <TransliterationToggle />
-        </div>
-      </nav>
+      {renderMenuToggle()}
+      {renderNavmenu()}
     </>
   );
 };
