a2c1ad9d162c8ad4ecd8e0b2ee4a751a17c697c1
Ravi S. Rāmphal <rramphal@gmail.com>
2024-12-10 16:10:06 -0600

    format Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,41 +3,39 @@ import { FunctionComponent } from 'react';
 import Image from "next/image";
 import Link from 'next/link';
 
+import classnames from 'classnames';
+
 import styles from './Footer.module.scss';
 
 const Footer: FunctionComponent = () => {
   return (<>
     <div className={styles.divider}></div>
+
     <footer className={styles.footer}>
-      <div className={styles.logobox}>
-        <div className={styles.item}>
-          <Image
-            alt="NESAR Logo"
-            src="/assets/images/logo-wordmark.png"
-            width={240}
-            height={53}
-            quality={90}
-            style={{
-              maxWidth: "100%",
-              height: "auto"
-            }} />
-        </div>
-        <div className={styles.linkbox}>
-          <p>ISSN: 2834-3875</p>
-          <p><Link href="/privacy">Privacy Policy</Link></p>
-        </div>
-      </div>
       <div className={styles.item}>
         <Image
-          alt="University of Chicago Library Logo"
+          className={styles.image}
+          src="/assets/images/logo-wordmark.png"
+          alt="NESAR Logo"
+          width={552}
+          height={122}
+        />
+      </div>
+
+      <div className={classnames(styles.item, styles.text)}>
+        <p>ISSN: 2834-3875</p>
+        <p><Link href="/privacy">Privacy Policy</Link></p>
+      </div>
+
+      <div className={styles.item}>
+        <Image
+          className={styles.image}
           src="/assets/images/uchicago-library.svg"
-          width={155}
-          height={43}
-          unoptimized={true}
-          style={{
-            maxWidth: "100%",
-            height: "auto"
-          }} />
+          alt="University of Chicago Library Logo"
+          width={207}
+          height={57}
+          unoptimized
+        />
       </div>
     </footer>
   </>);
